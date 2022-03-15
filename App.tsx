2487358111b47d05b89708173fa0b507642e02e1
Andreas Arnesson <andreas_osby@hotmail.com>
2022-03-15 16:35:32 +0100

    added routing and navigation (kmom02 part 1)

--- App.tsx
+++ App.tsx
@@ -3,16 +3,40 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import warehouse from './assets/warehouse.jpg';
 import Stock from './components/Stock.tsx';
+import Pick from "./components/Pick.tsx";
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
+const Tab = createBottomTabNavigator();
+const routeIcons = {
+  "Lager": "home",
+  "Plock": "list",
+};
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.base}>
+      {/* <View style={styles.base}>
         <Text style={{ color: '#33c', fontSize: 42 }}>Lager-Appen</Text>
         <Image source={warehouse} style={{ width: 320, height: 240 }} />
         <Stock />
         <StatusBar style="auto" />
-      </View>
+      </View> */}
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = routeIcons[route.name] || "alert";
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'blue',
+          tabBarInactiveTintColor: 'gray',
+        })}
+        >
+          <Tab.Screen name="Lager" component={Stock} />
+          <Tab.Screen name="Plock" component={Pick} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
